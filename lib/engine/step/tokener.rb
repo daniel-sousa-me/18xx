0813c91efb7205b79010cac1a330d96a55da54aa
daniel-sousa-me <github@daniel.sousa.me>
2020-10-30 00:52:14 -0400

    First part of the log improvements
    * Added logger class
    
    * Added types of logs for actions, messages and undo (consequences and
      events will be added later)
    
    * Show time of the action/message in the log
    
    * Links to a point in history from the log
    
    * Show small icon on log when some undos happened

--- lib/engine/step/tokener.rb
+++ lib/engine/step/tokener.rb
@@ -80,9 +80,9 @@ module Engine
         when :neutral
           entity.tokens.delete(token)
           token.corporation.tokens << token
-          @log << "#{tokener} places a neutral token on #{hex.name}#{price_log}"
+          @log.action! "places a neutral token on #{hex.name}#{price_log}"
         else
-          @log << "#{tokener} places a token on #{hex.name} (#{hex.location_name})#{price_log}"
+          @log.action! "places a token on #{hex.name} (#{hex.location_name})#{price_log}"
         end
 
         @round.tokened = true unless extra
