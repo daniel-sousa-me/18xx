f53c358dea035fcedff97dbfde4594f98af567c3
daniel-sousa-me <github@daniel.sousa.me>
2020-10-30 00:52:14 -0400

    First part of the log improvements
    * Added logger class
    
    * Added types of logs for actions, messages and undo (consequences and
      events will be added later)
    
    * Show time of the action/message in the log
    
    * Links to a point in history from the log
    
    * Show small icon on log when some undos happened

--- lib/engine/logger.rb
+++ lib/engine/logger.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 module Engine
-  class GameLog < Array
-    def initialize(game)
+  class Logger
+    attr_reader :lines
+
+    def initialize(game, lines = [])
       @game = game
+      @lines = lines
       @last_action_id = 0
     end
 
     def message!(action)
-      # TODO: Don't repeat this
-      self << { type: :undo } if @last_action_id + 1 < action.id
+      @lines << { type: :undo } if @last_action_id + 1 < action.id
       @last_action_id = action.id
 
-      # TODO: Make each type a different "Entry" class
-      self << {
+      @lines << {
         type: :message,
         id: action.id,
         created_at: action.created_at,
@@ -27,46 +28,40 @@ module Engine
     end
 
     def acted!(entity, message)
-      return self << message unless (action = @game&.current_action)
+      return @lines << message unless (action = @game&.current_action)
 
       entity ||= action.entity
-      entity_list = []
+      player = entity.owner unless entity.player?
 
-      while entity
-        entity_list << entity.name
-        break if entity.player?
+      return @lines << "#{entity.name} #{message}" if @last_action_id == action.id
 
-        entity = entity.owner
-      end
-
-      if @last_action_id == action.id
-        return self << "#{entity.name} #{message}" if entity
-
-        return self << message
-      end
-
-      self << { type: :undo } if @last_action_id + 1 < action.id
+      @lines << { type: :undo } if @last_action_id + 1 < action.id
       @last_action_id = action.id
 
-      self << {
+      @lines << {
         type: :action,
         id: action.id,
         created_at: action.created_at,
-        entity_list: entity_list,
+        entity: entity&.name,
+        player: player&.name,
         user: (@game.player_by_id(action.user)&.name || 'Owner' if action.user),
         message: message,
       }
     end
 
     def queue!
-      old_size = size
+      old_size = @lines.size
       yield
-      @queued_log = pop(size - old_size)
+      @queued_log = @lines.pop(@lines.size - old_size)
     end
 
     def flush!
-      @queued_log.each { |l| self << l }
+      @queued_log.each { |l| @lines << l }
       @queued_log = []
+    end
+
+    def <<(entry)
+      @lines << entry
     end
   end
 end
