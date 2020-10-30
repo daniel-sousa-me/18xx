b454de455533fdd0905db9ef6003d41618a68bf4
daniel-sousa-me <github@daniel.sousa.me>
2020-10-30 00:52:14 -0400

    First part of the log improvements
    * Added logger class
    
    * Added types of logs for actions, messages and undo (consequences and
      events will be added later)
    
    * Show time of the action/message in the log
    
    * Links to a point in history from the log
    
    * Show small icon on log when some undos happened

--- lib/engine/game/g_1817.rb
+++ lib/engine/game/g_1817.rb
@@ -441,7 +441,7 @@ module Engine
         short.buyable = false
         short.counts_for_limit = false
 
-        @log << "#{entity.name} shorts a #{percent}% " \
+        @log.action! "shorts a #{percent}% " \
           "share of #{corporation.name} for #{format_currency(price)}"
 
         @bank.spend(price, entity)
@@ -464,9 +464,7 @@ module Engine
         raise GameError, "Cannot take more than #{maximum_loans(entity)} loans" unless can_take_loan?(entity)
 
         price = entity.share_price.price
-        name = entity.name
-        name += " (#{entity.owner.name})" if @round.is_a?(Round::Stock)
-        @log << "#{name} takes a loan and receives #{format_currency(loan.amount)}"
+        @log.action! "takes a loan and receives #{format_currency(loan.amount)}"
         @bank.spend(loan.amount, entity)
         @stock_market.move_left(entity)
         log_share_price(entity, price)
