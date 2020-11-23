658e1a5f6ccc4504d6c911f7c47200467c10389f
daniel-sousa-me <github@daniel.sousa.me>
2020-11-23 00:30:54 +0000

    UI tweaks:
    Spreadsheet:
    
    * Show minors in 1846 and 18LA
    
    * Don't show 2 price columns if not necessary
    
    * In the player table, 0 shares are in half opacity and president in bold
    
    * Show shorts in 1817 (#2371)
    
    * Hide corporations that are closed or haven'd ipoed (#2390)
    
    * Increased some spacing
    
    * The order of corporations is always shown (#883)
    
    * Show reserved shares
    
    * Highlight current player during SRs instead of priority
    
    * 1817: Added loans to spreadsheet
    
    Cards:
    
    * Highlight current player
    
    * 1817: Show available shorts in corporation cards
    
    * 1817: Put company that is being auctioned before all others
    
    * 1817: Improved how corp size is shown (solves #2152)
    
    Improved look of the logos

--- lib/engine/game/base.rb
+++ lib/engine/game/base.rb
@@ -2088,7 +2088,7 @@ module Engine
 
       def corporation_size(_entity)
         # For display purposes is a corporation small, medium or large
-        :small
+        :large
       end
 
       def status_str(_corporation); end
