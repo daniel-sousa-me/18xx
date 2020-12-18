d8b6569502d7b0ec85dfe45979a732022a541edd
daniel-sousa-me <github@daniel.sousa.me>
2020-12-18 17:01:19 +0000

    Beginning of 18ui

--- assets/app/game_manager.rb
+++ assets/app/game_manager.rb
@@ -137,7 +137,9 @@ module GameManager
     store(:app_route, route, skip: @app_route == route)
 
     @connection.safe_get(game_url) do |data|
-      next `window.location = #{game_url}` if data.dig('settings', 'pin')
+      pin = data.dig('settings', 'pin')
+      url = "#{game_url}?pin=#{pin}"
+      next `window.location = url` if pin && !@app_route.include?(url)
 
       `window.history.replaceState(#{data.to_n}, #{@app_route}, #{@app_route})`
       store(:game_data, data, skip: false)
