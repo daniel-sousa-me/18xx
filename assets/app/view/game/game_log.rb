71096892ea4780f463c0d5d86691c8accb819def
daniel-sousa-me <github@daniel.sousa.me>
2021-01-06 12:02:09 +0000

    Made button margins symmetrical

--- assets/app/view/game/game_log.rb
+++ assets/app/view/game/game_log.rb
@@ -9,6 +9,14 @@ module View
       include Actionable
 
       needs :user
+      needs :show_messages, default: true, store: true
+      needs :log_level, default: 2, store: true
+
+      LOG_LEVEL_TEXT = {
+        2 => 'Hide actions',
+        1 => 'Show effects',
+        0 => 'Show actions',
+      }.freeze
 
       def render
         children = [
@@ -32,26 +40,36 @@ module View
         end
 
         if participant?
-          children << h(:div, {
-                          style: {
-                            margin: '0 0 1vmin 0',
-                            display: 'flex',
-                            flexDirection: 'row',
-                          },
-                        }, [
-            h(:span, {
-                style: {
-                  fontWeight: 'bold',
-                  margin: 'auto 0',
-                },
-              }, [@user['name'] + ':']),
-            h(:input,
-              style: {
-                marginLeft: '0.5rem',
-                flex: '1',
+          chat_line = [
+            h(:span, { style: {
+                fontWeight: 'bold',
+                margin: 'auto 0',
               },
-              on: { keyup: enter }),
-            ])
+}, [@user['name'] + ':']),
+            h(:input, style: {
+                marginLeft: '0.5rem',
+                height: '1.25rem',
+                flex: '1',
+              }, on: { keyup: enter }),
+            h(:button, {
+                style: { height: '100%' },
+                on: { click: -> { store(:show_messages, !@show_messages) } },
+              },
+              @show_messages ? 'Hide messages' : 'Show messages'),
+            h(:button, {
+                style: { height: '100%' },
+                on: { click: -> { store(:log_level, (@log_level + 1) % 3) } },
+              },
+              LOG_LEVEL_TEXT[@log_level]),
+          ]
+
+          children << h(:div, { style: {
+                          margin: '1vmin 0',
+                          display: 'flex',
+                          flexDirection: 'row',
+                          alignItems: 'center',
+                        },
+}, chat_line)
         end
 
         props = {
