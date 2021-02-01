73fa3b8c3bc95e98581d153b0cdcad635215f53d
daniel-sousa-me <github@daniel.sousa.me>
2021-02-01 10:57:30 +0000

    Add width to actions on the auction

--- assets/app/view/game/round/auction.rb
+++ assets/app/view/game/round/auction.rb
@@ -134,7 +134,7 @@ module View
             actions << h(:button, { on: { click: -> { assign(company) } } }, 'Reduce Price')
           end
 
-          h(:div, { style: { textAlign: 'center', margin: '1rem' } }, actions)
+          h(:div, { style: { textAlign: 'center', margin: '1rem', width: '20rem' } }, actions)
         end
 
         def render_company_actions(company)
@@ -260,7 +260,7 @@ module View
 
           minor_actions = [h(:button, { on: { click: choose } }, 'Choose')]
 
-          h(:div, { style: { textAlign: 'center', margin: '1rem' } }, minor_actions)
+          h(:div, { style: { textAlign: 'center', margin: '1rem', width: '20rem' } }, minor_actions)
         end
 
         def render_corporations
@@ -302,7 +302,7 @@ module View
 
           corp_actions = [h(:button, { on: { click: choose } }, 'Choose')]
 
-          h(:div, { style: { textAlign: 'center', margin: '1rem' } }, corp_actions)
+          h(:div, { style: { textAlign: 'center', margin: '1rem', width: '20rem' } }, corp_actions)
         end
 
         def hide!
